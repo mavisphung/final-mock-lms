5303af78776ef9e5938a0b93a17865189c639064
mavisphung <huypc2410@gmail.com>
2021-07-14 14:34:28 +0700

    First commit

---
+++ src/test/java/com/lms/project/MockLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
